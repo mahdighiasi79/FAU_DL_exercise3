b924447b46287161d65dc9fedf073d72c3459b5c
Mahdi Ghiasi <mmghf74@gmail.com>
2024-07-01 09:35:43 +0100

    dropout implemented

--- Layers/Dropout.py
+++ Layers/Dropout.py
@@ -1,4 +1,26 @@
-class Dropout:
+import copy
+import numpy as np
+
+from . import Base
+
+
+class Dropout(Base.BaseLayer):
 
     def __init__(self, probability):
+        super().__init__()
         self.probability = probability
+        self.mask = None
+
+    def forward(self, input_tensor):
+        output_tensor = copy.deepcopy(input_tensor)
+        if not self.testing_phase:
+            mask = np.random.choice(2, input_tensor.shape, p=[1 - self.probability, self.probability])
+            self.mask = mask
+            output_tensor *= mask
+            output_tensor /= self.probability
+        return output_tensor
+
+    def backward(self, error_tensor):
+        error_tensor *= self.mask
+        error_tensor /= self.probability
+        return error_tensor
